58656dc482ff8907264ac079af7ebf78ae9d18f2
Faizal Bin Ag Salleh <34163355+zalearth@users.noreply.github.com>
2017-12-14 14:57:49 +0800

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,38 @@
-## Put comments here that give an overall description of what your
-## functions do
+## this script contains 2 functions that caching invert matrix by 
+## using closure method from parent to child function
+ 
 
-## Write a short comment describing this function
+## set initial matrix value, getting matrix, setting inverse
+## matrix and getting inverse matrix to be use on cacheSolve function
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix <- function(mtrx = matrix()) {
+  invm <- NULL
+  
+  setm <- function(y) {
+    mtrx <<- y
+    invm <<- NULL
+  }
+  
+  getm <- function() mtrx
+  setinv <- function(inv) invm <<- inv
+  getinv <- function() invm
+  list(setm = setm, getm = getm, setinv = setinv, getinv = getinv)
 }
 
+## check whether inverse matrix cached. if not, the value assign converted 
+##to inverse matrix by using solve function
 
-## Write a short comment describing this function
+cacheSolve <- function(myfun, ...) {
+  
+  invm <- myfun$getinv()
+  if(!is.null(invm)) {
+    message("Invert Matrix already exist.")
+    return(invm)
+  }
+  
+  data <- myfun$getm()
+  invm <- solve(data)
+  myfun$setinv(invm)
+  invm
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
 }
